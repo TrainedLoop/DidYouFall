9ce007841fba2449e5b0d2487372c11d43af57e6
danielporto01 <ddanielporto@gmail.com>
2014-05-24 11:44:44 -0300

    SideBar

--- DidYouFall/App_Start/BundleConfig.cs
+++ DidYouFall/App_Start/BundleConfig.cs
@@ -13,7 +13,7 @@ namespace DidYouFall.App_Start
         {
             //LAYOUT
             bundles.Add(new ScriptBundle("~/bundles/Js/Layout").Include(
-                        "~/JavaScripts/Layout/jquery-ui-1.10.3.min.js",
+                        "~/JavaScripts/Layout/jquery-1.11.1.min.js",
                         "~/JavaScripts/Layout/bootstrap.min.js",
                         "~/JavaScripts/Layout/app.js"
                         ));
@@ -28,7 +28,6 @@ namespace DidYouFall.App_Start
 
             //SERVER REGISTER
             bundles.Add(new ScriptBundle("~/bundles/Js/Plugins/IpMask").Include(
-            "~/JavaScripts/Plugins/jquery-1.11.1.min.js",
             "~/JavaScripts/Plugins/Mask/IpMaskInt.js",
             "~/JavaScripts/Plugins/Mask/mask-plugin.js"
             ));
